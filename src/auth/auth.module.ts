02ce3d72e1f50aa1cc55d8d00d5c329b2526abf6
Marcelo Lima Gomes <marcelo.lima.gomes.23@gmail>
2023-12-20 15:51:17 -0300

    feat(auth) implement login,guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
-import { AuthService } from './auth/auth.service';
+
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: 'your-secret-key', // Troque pelo sua chave secreta
-      signOptions: { expiresIn: '1h' }, // Defina a expiração do token
-    }),
-  ],
-  providers: [AuthService, JwtStrategyService, JwtStrategyService],
+ 
+  providers: [AuthService],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
